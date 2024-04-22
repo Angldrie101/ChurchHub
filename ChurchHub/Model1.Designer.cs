4921e3a83d82cf052b3d916923c07016bde5ac0d
Angldrie101 <89821301+Angldrie101@users.noreply.github.com>
2024-04-23 00:57:51 +0800

    marbens

--- ChurchHub/Model1.Designer.cs
+++ ChurchHub/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Marvin\Documents\Visual Studio 2015\Projects\ChurchHub\ChurchHub\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ANGEL DORIE\OneDrive\Desktop\Documents\GitHub\ChurchHub\ChurchHub\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
